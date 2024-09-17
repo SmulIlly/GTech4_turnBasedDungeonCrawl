7cee11835542caefd4113216033ad37a246c1ff7
SmulIlly <smul.capa@gmail.com>
2024-09-17 10:42:26 +0200

    Game Input

--- G-Tech4_Projet1/Game.cpp
+++ G-Tech4_Projet1/Game.cpp
@@ -1,8 +1,11 @@
 #include "Game.h"
 #include "Level.h"
+#include "Input.h"
+#include "Keyboard.h"
 
 Game::Game()
 {
+
 }
 
 Game::~Game()
@@ -11,9 +14,13 @@ Game::~Game()
 
 void Game::Run()
 {
+	InputControl* inputControl = new Keyboard();
+
 	Level Level1(10,10);
 
+	Level1.InputUpdate(inputControl);
 	Level1.UpdateGrid();
+	
 }
 
 
